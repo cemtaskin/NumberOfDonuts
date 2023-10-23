46dd6d032381a1e5f75d7a8736600c18dd38b43f
CEM TASKIN <cemtaskin82@gmail.com>
2023-10-23 11:58:02 +0300

    Intitial Commit

--- NumberOfDonuts/ViewController.swift
+++ NumberOfDonuts/ViewController.swift
@@ -8,11 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var value : Int = 0
+    @IBOutlet var  lblCount : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func getLabelValue (){
+        value = Int(lblCount.text!)!
+    }
+    
+    func setLabelValue (){
+        lblCount?.text = "\(value)"
+    }
+    
+    
+    @IBAction func plus (){
+        getLabelValue()
+        value+=1
+        setLabelValue()
+    }
+    
+    @IBAction func minus (){
+        getLabelValue()
+        value-=1
+        setLabelValue()
+    }
+    
+    @IBAction func clear (){
+        value = 0
+        setLabelValue()
+    }
+    
+    @IBAction func twoTimes (){
+        getLabelValue()
+        value = value * 2
+        setLabelValue()    }
+    
 
 
 }
